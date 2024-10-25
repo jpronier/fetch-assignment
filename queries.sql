cd422b431c5153a4481aba9234c901ad1d32034d
Julie Pronier <julie@pronier.com>
2024-10-25 13:38:41 -0400

    work on queries file - q3,4,5,6

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- What are the top 5 brands by receipts scanned for most recent month?
 
--- How do we define being a top brand? 
--- First, let's decide that we want to look at the total number of items purchased per brand
+/* How do we define being a top brand? 
+First, let's decide that we want to look at the total number of items purchased per brand */
 SELECT 
     brandCode,
     brandName,
@@ -12,13 +12,17 @@ GROUP BY brandCode, brandName
 ORDER BY total_purchased_items DESC
 LIMIT 5;
 
--- Now, this interpretation can lead to some issues. If a customer buys 100 items from a single brand A, but 100 customers
--- buy 1 item from brand B, brand A will be ranked higher than brand B, which is not a good interpretation of what a top brand is.
+/*
+Now, this interpretation can lead to some issues. If a customer buys 100 items 
+from a single brand A, but 100 customers buy 1 item from brand B, brand A will 
+be ranked higher than brand B, which is not a good interpretation of what a top 
+brand is.
 
--- So now, let's decide that we want to look at the total number of receipts per brand and then the total number of items 
--- purchased per brand if there are multiple brands with the same number of receipts.
+So now, let's decide that we want to look at the total number of receipts per 
+brand and then the total number of items purchased per brand if there are 
+multiple brands with the same number of receipts.
+*/
 
--- What are the top 5 brands by receipts scanned for most recent month?
 SELECT 
     i.brandCode,
     b.name AS brandName,
@@ -66,3 +70,56 @@ FROM current_month c
 LEFT JOIN previous_month p ON c.brandCode = p.brandCode
 WHERE c.rank <= 5 OR p.rank <= 5
 ORDER BY c.rank, p.rank;
+
+
+-- When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+SELECT 
+    rewardsReceiptStatus,
+    AVG(totalSpent) AS average_spend
+FROM receipts
+WHERE rewardsReceiptStatus IN ('Accepted', 'Rejected')
+GROUP BY rewardsReceiptStatus
+ORDER BY AVG(totalSpent) DESC
+LIMIT 1;
+
+-- When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+SELECT 
+    r.rewardsReceiptStatus,
+    SUM(i.quantityPurchased) AS total_items_purchased
+FROM receipts r
+JOIN items i ON r.receipt_id = i.receipt_id
+WHERE r.rewardsReceiptStatus IN ('Accepted', 'Rejected')
+GROUP BY r.rewardsReceiptStatus
+ORDER BY SUM(i.quantityPurchased) DESC
+LIMIT 1;
+
+-- Which brand has the most spend among users who were created within the past 6 months?
+
+SELECT 
+    b.name AS brand_name,
+    SUM(r.totalSpent) AS total_spend
+FROM users u
+JOIN receipts r ON u.user_id = r.user_id
+JOIN items i ON r.receipt_id = i.receipt_id
+JOIN brands b ON i.brandCode = b.brandCode
+WHERE u.createdDate >= DATEADD(month, -6, GETDATE())
+GROUP BY b.name
+ORDER BY total_spend DESC
+LIMIT 1; 
+
+-- Which brand has the most transactions among users who were created within the past 6 months?
+
+
+SELECT 
+    b.name AS brand_name,
+    COUNT(DISTINCT r.receipt_id) AS transaction_count
+FROM users u
+JOIN receipts r ON u.user_id = r.user_id
+JOIN items i ON r.receipt_id = i.receipt_id
+JOIN brands b ON i.brandCode = b.brandCode
+WHERE u.createdDate >= DATEADD(month, -6, GETDATE())
+GROUP BY b.name
+ORDER BY transaction_count DESC
+LIMIT 1; 
